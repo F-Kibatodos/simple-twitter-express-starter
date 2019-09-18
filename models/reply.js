654099ffead965b9b6386ee4dca1febf0846fd96
Brian.L <brian9500885@gmail.com>
2019-09-18 18:03:41 +0800

    test1

--- models/reply.js
+++ models/reply.js
@@ -1,8 +1,17 @@
-'use strict';
+'use strict'
 module.exports = (sequelize, DataTypes) => {
-  const Reply = sequelize.define('Reply', {
-  }, {});
+  const Reply = sequelize.define(
+    'Reply',
+    {},
+    {
+      TweetId: DataTypes.INTEGER,
+      UserId: DataTypes.INTEGER,
+      comment: DataTypes.STRING
+    }
+  )
   Reply.associate = function(models) {
-  };
-  return Reply;
-};
+    Reply.belongsTo(models.Tweet)
+    Reply.belongsTo(models.User)
+  }
+  return Reply
+}
